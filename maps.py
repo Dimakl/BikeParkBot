292ac33502abc4628aaef137641d773d868acae0
zello-hello <zellohello0@gmail.com>
2019-12-07 17:42:17 +0300

    find n nearest

--- maps.py
+++ maps.py
@@ -2,7 +2,6 @@ import logging
 import geopy
 import geopy.distance
 logger = logging.getLogger(__name__)
-from config import APP_CODE, APP_ID, WIDTH, HEIGHT, PPI, LINE_WIDTH, STREET_VIEW_TYPE, LINE_COLOR
 
 
 class Path:
@@ -19,27 +18,12 @@ class Path:
     generate map image with route
     """
     def gen_route_to(self, parking_coords):
-        target_url = "https://image.maps.api.here.com/mia/1.6/routing"
+        pass
 
-        d = "lc=1652B4&" + "lw=6&t=0&"
-
-        params = {
-            "app_id": APP_ID,
-            "app_code": APP_CODE,
-            "waypoint0": "{0}, {1}".format(*self.coords),
-            "waypoint1": "{0}, {1}".format(*parking_coords),
-            "w": WIDTH,
-            "h": HEIGHT,
-            "ppi": PPI,
-            "lw": LINE_WIDTH,
-            "t": STREET_VIEW_TYPE,
-            "lc": LINE_COLOR
-
-        }
-
-    def find_n_nearest(self, n, parkings):
-        pts = [ geopy.Point(p[0],p[1]) for p in parkings]
-        onept = geopy.Point(self.coords[0],self.coords[1])
-        alldist = [ (p,geopy.distance.distance(p, onept).km) for p in pts ]
-        nearest_point = sorted(alldist, key=lambda x: (x[1]))[0]
+    def find_n_nearest(n, parkings, coordinate):
+        # the solution
+        pts = [geopy.Point(p[0], p[1]) for p in ccoordinate_list]
+        onept = geopy.Point(coordinate[0], coordinate[1])
+        alldist = [(p, geopy.distance.distance(p, onept).km) for p in pts]
+        nearest_point = sorted(alldist, key=lambda x: (x[1]))[:n]
         return [[k[0][0], k[0][1]] for k in nearest_point]
