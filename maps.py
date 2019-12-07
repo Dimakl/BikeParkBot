e9dd9c7adcf314843594c1051355aee96c50608f
zello-hello <zellohello0@gmail.com>
2019-12-07 17:18:36 +0300

    Adding find nearest

--- maps.py
+++ maps.py
@@ -1,4 +1,6 @@
 import logging
+import geopy
+import geopy.distance
 logger = logging.getLogger(__name__)
 
 
@@ -18,6 +20,13 @@ class Path:
     def gen_route_to(self, parking_coords):
         pass
 
-    def find_n_nearest(self, n):
+    def find_n_nearest(self, n, parkings):
+        # your data
+        ccoordinate_list = [(37.7718513, 55.639926), (37.655829000192, 55.71449030019)]
+        coordinate = (37.52508949, 55.75611045)
+        # the solution
+        pts = [ geopy.Point(p[0],p[1]) for p in ccoordinate_list ]
+        onept = geopy.Point(coordinate[0],coordinate[1])
+        alldist = [ (p,geopy.distance.distance(p, onept).km) for p in pts ]
+        nearest_point = sorted(alldist, key=lambda x: (x[1]))[0] #
         pass
-
