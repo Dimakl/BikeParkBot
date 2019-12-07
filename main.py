f31083f8e96fb1b9789ba0070adb993748106cad
Dimakl <dimakid123@gmail.com>
2019-12-07 16:13:39 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,70 +7,44 @@ Basic example for a bot that uses inline keyboards.
 """
 import logging
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+import telegram
+from telegram.ext import Updater, Filters, MessageHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data="1", request_location=True)]]
+def request_location(update, context):
+    location_keyboard = telegram.KeyboardButton(text="send_location", request_location=True)
+    reply_markup = telegram.ReplyKeyboardMarkup([[location_keyboard]])
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-
-def button(update, context):
-    query = update.callback_query
-    print(update.message.location)
-    query.edit_message_text(text="Selected option: {}".format(query.data))
+    update.message.reply_text('Отправьте локацию:', reply_markup=reply_markup)
 
 
-def help(update, context):
-    update.message.reply_text("Use /start to test this bot.")
-
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def compute_location(update, context):
+    location = update.message.location
+    print(location)
 
 
 def main():
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
 
     REQUEST_KWARGS = {
         'proxy_url': 'socks5://185.73.240.95:1080',
-        # Optional, if you need authentication:
+
         'urllib3_proxy_kwargs': {
             'username': 'k4proxy',
             'password': 'sosipisos8464'
         }
     }
 
-    custom_keyboard = [['top-left', 'top-right'],
-                       ['bottom-left', 'bottom-right']]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(chat_id=chat_id,
-                     text="Custom Keyboard Test",
-                     reply_markup=reply_markup)
-
     updater = Updater("956994519:AAHNHMkrKR3D4ppbe-mLOCdVIDM1aHBFSuQ", use_context=True, request_kwargs=REQUEST_KWARGS)
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_error_handler(error)
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, request_location))
+    updater.dispatcher.add_handler(MessageHandler(Filters.location, compute_location))
 
-    # Start the Bot
     updater.start_polling()
 
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
     updater.idle()
 
 
